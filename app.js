0a13cbbeeda87a7b71076496d6c3dfc9e2461026
Kalyan Vedala <svedala23@gmail.com>
2024-08-01 05:15:06 -0700

    Use cors and body parser

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ require("dotenv").config();
 const express = require('express');
 const connectDB = require("./config/db");
 const routes = require("./routes/api/books");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-connectDB();
+app.use(cors({ origin: true, credentials: true }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/books", routes);
 
+connectDB();
+
 app.get('/', (req, res) => res.send('Hello world!'));
-
 const port = process.env.PORT || 8082;
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
